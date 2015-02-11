e227495a2b5a815011aad42ce7cbb88f665cf2fb
Mathieu Baudet <mathieubaudet@fb.com>
2015-02-10 21:55:38 -0800

    [Deduplication] Ignore namespaces and (toplevel) class specializations
    Summary:
    [Deduplication] Ignore namespaces and (toplevel) class specializations
    
    This diff adds more declarations to the list that should not be skipped (even if we have indexed the header file already).
    
    Test Plan: make -C libtooling test
    
    Reviewers: hansonw, adek
    
    Reviewed By: adek
    
    Differential Revision: https://phabricator.fb.com/D1840557
    
    Tasks: 4598028
    
    Signature: t1:1840557:1423675457:1b2ceebfe5de814b7aa35dd3768294f7e2794956

--- libtooling/FileUtils.cpp
+++ libtooling/FileUtils.cpp
@@ -15,6 +15,8 @@
 #include <vector>
 
 #include <clang/AST/Decl.h>
+#include <clang/AST/DeclCXX.h>
+#include <clang/AST/DeclTemplate.h>
 #include <llvm/Support/Path.h>
 
 #include "FileUtils.h"
@@ -61,8 +63,18 @@ namespace FileUtils {
     if (!DC || !clang::isa<clang::TranslationUnitDecl>(*DC)) {
       return true;
     }
-    // Avoid in particular LinkageSpec which can have misleading locations.
+    // Skip only NamedDecl's. Avoid in particular LinkageSpec's which can have misleading locations.
     if (!clang::isa<clang::NamedDecl>(Decl)) {
+      return true;
+    }
+    // Do not skip Namespace, ClassTemplate(Partial)Specialization, Using, UsingShadow, CXXRecord declarations.
+    // (CXXRecord mostly because they can contain UsingShadow declarations.)
+    if (clang::isa<clang::NamespaceDecl>(Decl)
+        || clang::isa<clang::ClassTemplateSpecializationDecl>(Decl)
+        || clang::isa<clang::UsingDecl>(Decl)
+        || clang::isa<clang::UsingShadowDecl>(Decl)
+        || clang::isa<clang::CXXRecordDecl>(Decl)
+        ) {
       return true;
     }
     clang::SourceLocation SpellingLoc = sourceManager.getSpellingLoc(Decl.getLocation());
