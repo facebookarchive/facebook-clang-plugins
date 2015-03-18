74a3f7c2deb361f8954acec0af1209bd6bb50f09
Mathieu Baudet <mathieubaudet@fb.com>
2015-03-18 11:56:08 -0700

    [nit] Remove C string
    Summary: [nit] Remove C string
    
    Test Plan: `make -C libtooling DEBUG=1 test`
    
    Reviewers: hansonw
    
    Reviewed By: hansonw
    
    Differential Revision: https://phabricator.fb.com/D1924333
    
    Tasks: 6388301
    
    Signature: t1:1924333:1426706655:d42ffd4b1b0b3711db9072d4c7a97e1aec0bff1c

--- libtooling/FileServices.cpp
+++ libtooling/FileServices.cpp
@@ -21,13 +21,9 @@
 
 namespace FileServices {
 
-  char *create_filename(const std::string &prefix, const std::string &servicePath, const std::string &key) {
+  std::string create_filename(const std::string &prefix, const std::string &servicePath, const std::string &key) {
     int len = servicePath.length() + prefix.length() + 19;
-    char *file = (char *)malloc(len);
-    if (!file) {
-      fprintf(stderr, "Memory allocation failed.\n");
-      exit(-1);
-    }
+    char file[len];
 
     // NOTE: This requires sizeof(size_t) == 8 and a correct implementation of std::hash.
     std::hash<std::string> strhash;
@@ -44,51 +40,47 @@ namespace FileServices {
       return I->second;
     }
 
-    char *file = create_filename("lock", servicePath, key);
-    int fd = open(file, O_CREAT | O_EXCL, 0644);
+    std::string file = create_filename("lock", servicePath, key);
+    int fd = open(file.c_str(), O_CREAT | O_EXCL, 0644);
     bool result = (fd > 0);
     if (result) {
       close(fd);
 #ifdef DEBUG
       // Re-open to write the key being tagged.
-      int fd = open(file, O_WRONLY, 0644);
+      int fd = open(file.c_str(), O_WRONLY, 0644);
       dprintf(fd, "%s\n", key.c_str());
       close(fd);
 #endif
     }
 
-    free(file);
     cache[key] = result;
     return result;
   }
 
   const std::string &TranslationService::findOriginalFile(const std::string &path) {
     auto I = cache.find(path);
-    auto E = cache.end();
-    if (I != E) {
+    if (I != cache.end()) {
       return I->second;
     }
 
-    char *file = create_filename("copy", servicePath, path);
-    std::string result = path;
+    std::string &result = cache[path];
+    std::string file = create_filename("copy", servicePath, path);
     std::ifstream fin(file);
     if (fin.is_open()) {
       // Read the real path behind a copied path.
       std::getline(fin, result);
+    } else {
+      result = path;
     }
 
-    free(file);
-    cache[path] = result;
-    return cache[path];
+    return result;
   }
 
   void TranslationService::recordCopiedFile(const std::string &copiedPath, const std::string &realPath) {
-    char *file = create_filename("copy", servicePath, copiedPath);
-    int fd = open(file, O_CREAT | O_EXCL | O_WRONLY, 0644);
+    std::string file = create_filename("copy", servicePath, copiedPath);
+    int fd = open(file.c_str(), O_CREAT | O_EXCL | O_WRONLY, 0644);
     dprintf(fd, "%s\n", realPath.c_str());
     close(fd);
-
-    free(file);
   }
 
 }
