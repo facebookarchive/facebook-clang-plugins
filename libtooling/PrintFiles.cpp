18099622a50f1d66afdad8b4c8d8259b3dac9611
Mathieu Baudet <mathieubaudet@fb.com>
2016-11-01 12:00:25 -0700

    [libtooling] Create test plugin `PrintFiles`
    Summary:
    Make sure NoOpenSimplePluginASTAction is tested in this repo.
    
    @depends on D4111574
    
    Test Plan: `make -C libtooling test`
    
    Reviewers: jul, akotulski
    
    Reviewed By: akotulski
    
    Differential Revision: https://phabricator.intern.facebook.com/D4112375
    
    Signature: t1:4112375:1478028127:60e8cecfb45a175d92d5759267c17165e1c920c3

---
+++ libtooling/PrintFiles.cpp
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+
+#include <clang/AST/ASTConsumer.h>
+#include <clang/Frontend/FrontendPluginRegistry.h>
+#include <iostream>
+
+#include "SimplePluginASTAction.h"
+
+using namespace clang;
+
+namespace {
+
+class PrintFilesConsumer : public ASTConsumer {
+
+ public:
+  PrintFilesConsumer(
+      const CompilerInstance &CI,
+      std::unique_ptr<ASTPluginLib::PluginASTOptionsBase> &&Options,
+      std::unique_ptr<std::string> outputFile) {
+    std::cout << "Input file: " << Options->inputFile.getFile().str() << "\n";
+    std::cout << "Input file kind: " << Options->inputFile.getKind() << "\n";
+    if (outputFile) {
+      std::cout << "Output file: " << *outputFile << "\n";
+    }
+  }
+
+};
+
+typedef ASTPluginLib::NoOpenSimplePluginASTAction<PrintFilesConsumer>
+    PrintFiles;
+
+}
+
+static FrontendPluginRegistry::Add<PrintFiles> X(
+    "PrintFiles", "Print source files and their kinds");
